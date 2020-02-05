fba9df1f23f64c089bb14c6202ef63a14b6d01fa
grey102 <60707551+grey102@users.noreply.github.com>
2020-02-05 20:11:30 +0300

    Add project

---
+++ Assets/Scripts/BomberMan.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BomberMan : MonoBehaviour
+{
+    private float movementSpeed = 5f;
+
+    //Prefabs
+    public GameObject bombPrefab;
+
+    void Update()
+    {
+        Move();
+        //AxisMove();
+    }
+
+    void Move()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position = transform.position + new Vector3(-0.06f, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position = transform.position + new Vector3(0.06f, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position = transform.position + new Vector3(0, 0.06f, 0);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position = transform.position + new Vector3(0, -0.06f, 0);
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            DropBomb();
+        }
+    }
+
+    void AxisMove()
+    {
+        //get the Input from Horizontal axis
+        float horizontalInput = Input.GetAxis("Horizontal");
+        //get the Input from Vertical axis
+        float verticalInput = Input.GetAxis("Vertical");
+
+        //update the position
+        transform.position = transform.position + new Vector3(horizontalInput * movementSpeed * Time.deltaTime, verticalInput * movementSpeed * Time.deltaTime, 0);
+
+        //output to log the position change
+        Debug.Log(transform.position);
+    }
+
+    void DropBomb()
+    {
+
+        Instantiate(bombPrefab, new Vector2(Mathf.Round(transform.position.x), Mathf.Round(transform.position.y)), transform.rotation);
+
+    }
+}
+
---
+++ Assets/Scripts/DestroyBomb.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyBomb : MonoBehaviour
+{
+    public GameObject explosionPrefab;
+
+    void Start()
+    {
+        Invoke("Explode", 3f);
+    }
+
+    void Update()
+    {
+
+    }
+
+    void Explode()
+    {
+        Instantiate(explosionPrefab, transform.position, Quaternion.identity); //1
+
+        GetComponent<SpriteRenderer>().enabled = false; //2
+        transform.Find("Collider").gameObject.SetActive(false); //3
+        Destroy(gameObject, .3f); //4
+    }
+}
